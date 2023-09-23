4fd0f6cfcf5e32ba12ccb0a146aeae3cadb637fc
najah <najahaboobacker79@gmail.com>
2023-09-23 15:54:03 +0530

    23

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 import Article from './components/article/Article';
 import navbar from './components/navbar/navbar';
 import brand from './components/brand/brand';
 import feature from './components/feature/feature';
 import cta from './components/cta/cta';
-import{footer,header, possiblity, whatgpt3,features,blog} from "./containers"
+ import footer from './containers/footer/footer';
+ import features from './containers/features/features';
+ import blog from './containers/blog/blog';
+ import header from './containers/header/header';
+ import whatgpt3 from'./containers/whatGPT3/whatgpt3';
+ import possiblity from './containers/possiblity/possiblity';
 function App() {
   return (
-    <div>
+    <div className='App'>
+      <div className='gradient_bg'>
+        <navbar/>
+        <header/>
+      </div>
+      <div>
+        <brand/>
+        <whatgpt3/>
+        <features/>
+        <possiblity/>
+        <cta/>
+        <blog/>
+        <footer/>
+
+      </div>
+
       <h1>ctp-3</h1>
     </div>
   )
--- src/containers/footer/footer.js
+++ src/containers/footer/footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import "/footer.css"
+import "./footer.css"
 function footer() {
   return (
     <div>
+    
       
     </div>
   )
